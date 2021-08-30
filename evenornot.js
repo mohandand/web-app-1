4a3e3ee625a74bf41671f65375729e62ee5c7680
mohandand <mohandand@gmail.com>
2021-08-29 18:24:15 -0700

    Added evenorodd program

---
+++ evenornot.js
@@ -0,0 +1,13 @@
+function even_or_odd(number) {
+    var number;
+    console.log(number);
+    if ( number % 2 == 0)
+       console.log(+ number + " is Even");
+    else{
+      console.log('Odd');
+    }
+  }
+
+  console.log(even_or_odd(2));
+  console.log(even_or_odd(7));
+  console.log(even_or_odd(-42));
--- practice.js
+++ practice.js
@@ -3,3 +3,4 @@ res.innerHTML = 'tesxt cnaged';
 console.log(res);
 var age = prompt('Enter your age..?');
 document.getElementById('headerTwo').innerHTML  = age;
+// <script src='practice.js'></script>
