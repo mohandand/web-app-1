c2f155426db9ca4315e2124bfb43a97bb35b19b0
mohandand <mohandand@gmail.com>
2021-09-06 21:58:56 -0700

    updated new code

--- practice.js
+++ practice.js
@@ -1,5 +1,5 @@
 var res = document.getElementById('headerOne');
-res.innerHTML = 'text changed Dynamically';
+res.innerHTML = 'text changed Dynamically with javascript';
 console.log(res);
 var age = prompt('Enter your age....?');
 document.getElementById('headerTwo').innerHTML  = age;
