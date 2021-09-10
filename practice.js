30f1cca084b5a083629952caa8dc56cf1a81cd52
mohandand <mohandand@gmail.com>
2021-09-10 16:49:02 -0700

    Added one more program

--- practice.js
+++ practice.js
@@ -3,4 +3,13 @@ res.innerHTML = 'text changed Dynamically with javascript';
 console.log(res);
 var age = prompt('Enter you'r age....?');
 document.getElementById('headerTwo').innerHTML  = age;
-// <script src='practice.js'></script>
+// <script src='practice.js'>
+function between(a, b) {
+            var ar1=[];
+            var i =a ;
+           for(var i; i<=b ;i++)
+             ar1.push(i);
+             return ar1;
+        }
+        console.log(between(3,8));
+</script>
