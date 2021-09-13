19b292b9869b818fde29c7f91479be01cbbe7f8c
mohandand <mohandand@gmail.com>
2021-09-12 19:56:35 -0700

    updated code

--- evenornot.js
+++ evenornot.js
@@ -13,4 +13,4 @@ function even_or_odd(number) {
   console.log(even_or_odd(-42));
 console.log(even_or_odd(5));
 console.log(even_or_odd(11));
-console.log(even_or_odd(10));
+console.log(even_or_odd(14));
