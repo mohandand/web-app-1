64f414a712055ace1887a0b14990f288465755b5
mohandand <mohandand@gmail.com>
2021-09-16 20:13:20 -0700

    added let and const

--- variables.js
+++ variables.js
@@ -4,3 +4,23 @@
  console.log(a);
  var a = 10 ;
  console.log(a);
+
+ /* but if we use let or const it wont allow us to use before intialization and it will through error. 
+ because hoisting wont happen for let and const */
+
+ // let keyword will have block level scop
+
+ let weather = 'sunny';
+
+ function changeWeather(){
+     let weather = 'rainy';
+     console.log('wether in function level : ' +weather );
+ }
+changeWeather();
+console.log(weather);   
+
+/* in above first it will print rainy as it block level but where as 
+below statment print sunny as it declraed globally.
+ but if use const it wont allow in inside function also */
+
+ 
