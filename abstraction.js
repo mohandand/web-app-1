3fae7fb33c0d068bd115e4b476888b3eadc147fa
mohandand <mohandand@gmail.com>
2021-09-17 17:43:36 -0700

    updated code

--- abstraction.js
+++ abstraction.js
@@ -6,3 +6,32 @@ For example if we create array and if u put arraname and dot then we will get lo
  we dont know where its defeination is. And genreally in java we use interface to implemmnt this concept 
  here we dont have interface so we use prototype. */
 
+//
+
+var father = {
+    name:'Father',
+    sing(){
+        console.log('sing');
+    },
+    eat: ()=>{
+        console.log('he can eat');
+    
+    }
+}
+
+var child = {
+    name:'Father',
+    sing(){
+        console.log('sing');
+    },
+    bat: ()=>{
+        console.log('he can bat');
+    
+    }
+}
+child.__proto__ = father;
+
+for(let p in child)
+{
+    console.log(p + "" +child.hasOwnProperty(p));
+}
