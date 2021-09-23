223a2e2a7cc4dfffbc9f98e5c89355ab55e76ca2
mohandand <mohandand@gmail.com>
2021-09-23 10:28:21 -0700

    updated overridding conxept program

---
+++ overridding.js
@@ -0,0 +1,21 @@
+class Parent
+{
+    live(){
+        console.log('He lives in India');
+    }
+}
+
+class Child extends Parent
+
+{   
+    live()
+    {
+        console.log('He lives in india');
+    }
+}
+
+var obj = new Child();
+obj.live();
+
+// we get output as overridding
+
