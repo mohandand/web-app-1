ab530b1c3acbb4b449796a7c0e91bd57c18fcd03
mohandand <mohandand@gmail.com>
2021-09-22 23:24:25 -0700

    updated code

---
+++ firstletteruppercase.js
@@ -0,0 +1,15 @@
+  var str = "How can mirrors be real if our eyes aren't real";
+  const words = str.split(" ");
+  console.log(words);
+  var case1 = words.map((i) => i.charAt(0).toUpperCase() + i.slice(1));
+//   function titleCase(stri)
+//   {
+//   for(var i=0 ;i<str.length;i++)
+//   {
+//       var case1 = stri[i].charAt(0).toUpperCase();
+//       var ar1=[];
+//       ar1.push(case1);
+//   }
+//   return ar1;
+// }
+  console.log(case1);  
