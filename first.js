fae339375f1a27b1e046f3fd3bf53a297841711d
mohandand <mohandand@gmail.com>
2021-09-23 20:08:53 -0700

    updated code

--- first.js
+++ first.js
@@ -1,7 +1,14 @@
 console.log('Connected');
 
-var button = document.querySelector('button');
-button.addEventListener('click',function(btn)
+// var button = document.querySelector('button');
+// button.addEventListener('click',function(btn)
+// {
+//     console.log('Button was clicked');
+// });
+
+var lis = document.querySelectorAll('li');
+
+for(var i=0;i<lis.length;i++)
 {
-    console.log('Button was clicked');
-});
+    lis[i].addEventListener('click',function(id){this.style.color="red";});
+}
